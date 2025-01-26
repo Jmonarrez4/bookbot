203fcbe6f3f9e567f27890471ab40065dd9f9f2d
Jmonarrez4 <juan.monarrez728@gmail.com>
2025-01-26 15:44:05 -0600

    D: updated main.py

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ def main():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
     word_count(contents)
+    count_characters(contents)
 
 def word_count(text):
     words = text.split()
     print(len(words))
+
+def count_characters(text):
+    characters = {}
+    text = text.lower()
+    for character in text:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    print(characters)
 
 main()
