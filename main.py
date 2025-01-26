a514f88aa5dd52dee29fc8951381defa69cefb3d
Jmonarrez4 <juan.monarrez728@gmail.com>
2025-01-26 15:22:20 -0600

    C: update main.py) q

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
-    print(contents)
+    word_count(contents)
+
+def word_count(text):
+    words = text.split()
+    print(len(words))
 
 main()
